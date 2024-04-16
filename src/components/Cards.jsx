30a72b63d0ff9028349a2711429747ac24519867
Chris Najman <christopher.najman@gmail.com>
2024-04-16 16:49:38 +0100

    Moved card data to its own file /src/cardData.js

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,43 +1,25 @@
 import Card from "./Card"
 
+import cardData from "../cardData"
+
 function Cards() {
-  const cards = [
-    {
-      id: 1,
-      extraInfo: "Sold out",
-      image: "life-lessons.png",
-      imageAlt: "Katie Zoferies by the pool",
-      star: true,
-      rating: 5.0,
-      likes: 6,
-      country: "USA",
-      text: "Life lessons with Katie Zoferies",
-      price: 136,
-      unit: "person",
-    },
-    {
-      id: 2,
-      extraInfo: "Online",
-      image: "wedding-photography.png",
-      imageAlt: "Woman modelling a wedding dress",
-      star: true,
-      rating: 5.0,
-      likes: 30,
-      country: "USA",
-      text: "Learn wedding photography",
-      price: 125,
-      unit: "person",
-    },
-    {
-      id: 3,
-      image: "mountain-bike.png",
-      imageAlt: "Mountain bike with view",
-      country: "USA",
-      text: "Group mountain biking",
-      price: 250,
-      unit: "couple",
-    },
-  ]
+  const cards = cardData.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        extraInfo={card.extraInfo}
+        src={card.image}
+        alt={card.imageAlt}
+        star={card.star}
+        rating={card.rating}
+        likes={card.likes}
+        country={card.country}
+        text={card.text}
+        price={card.price}
+        unit={card.unit}
+      />
+    )
+  })
 
   return (
     <section
@@ -46,33 +28,9 @@ function Cards() {
       aria-labelledby="card-list"
       tabIndex="0"
     >
-      <ul id="card-list">
-        {cards.map((card) => {
-          return (
-            <Card
-              key={card.id}
-              extraInfo={card.extraInfo}
-              src={card.image}
-              alt={card.imageAlt}
-              star={card.star}
-              rating={card.rating}
-              likes={card.likes}
-              country={card.country}
-              text={card.text}
-              price={card.price}
-              unit={card.unit}
-            />
-          )
-        })}
-      </ul>
+      <ul id="card-list">{cards}</ul>
     </section>
   )
 }
 
 export default Cards
-
-/*
-
-
-
-*/
